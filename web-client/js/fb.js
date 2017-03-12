5623d1b4b032ea628e1c5b9bb80a4ef2e3f5b6ea
Mantas <M.Serapinas@gmail.com>
2017-03-12 06:37:13 +0000

    fix

---
+++ web-client/js/fb.js
@@ -0,0 +1,64 @@
+/**
+ * Created by Mantas on 12/03/2017.
+ */
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '969322433241610',
+        xfbml      : true,
+        version    : 'v2.8'
+    });
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function login() {
+    console.log("login");
+    FB.login(function(response) {
+        if (response.status === 'connected') {
+            // Logged into your app and Facebook.
+            console.log(response);
+            checkLoginState();
+        } else {
+            // The person is not logged into this app or we are unable to tell.
+            console.log(response);
+        }
+    }, {scope: 'public_profile,email,user_friends'});
+}
+
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+        // Logged into your app and Facebook.
+        testAPI();
+    } else {
+        // The person is not logged into your app or we are unable to tell.
+        document.getElementById('status').innerHTML = 'Please log ' +
+            'into this app.';
+    }
+}
+
+function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+        console.log('Successful login for: ' + response);
+        loginApi(response);
+    });
+    window.location = "/Tracks/web-client/map.html"
+}
+
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+}
